9808608ebce756028bfa3d8a490a1a7e36fe76c7
Yuliya-Khovrych <ulia.voitishenko@gmail.com>
2022-11-13 12:33:56 +0200

    await

---
+++ src/js/modal-movie-info.js
@@ -0,0 +1,20 @@
+import { getMovieById } from './fetch-functions/get-movie-info';
+import { activateModalBtns } from './modal/activate-movie-modal';
+import { renderModalFilm } from './modal/render-modal';
+
+export async function onMovieCardClick(evt) {
+  evt.preventDefault();
+  const currentItem = evt.target.closest('li');
+  let id = +currentItem.dataset.id;
+
+  try {
+    document.querySelector('body').classList.add('modal-open');
+
+    const movieId = await getMovieById(id);
+    const movieInfo = movieId.data;
+    renderModalFilm({ movieInfo });
+    activateModalBtns();
+  } catch (error) {
+    console.log(error);
+  }
+}
